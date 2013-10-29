00256daf0f536d5c9f19aaa2619e043b7895fd6d
Philippe Blondin <r00tmac@r00tmac.local>
2013-10-28 21:27:14 -0400

    Add a locomotivjs bootstart to start app via node

---
+++ app.js
@@ -0,0 +1,14 @@
+// Locomotivejs Bootstrap
+
+var locomotive = require('locomotive'),
+ 		env = process.env.NODE_ENV || 'development',
+		port = process.env.PORT || 3000,
+		address = '0.0.0.0';
+
+locomotive.boot(__dirname, env, function(err, server) {
+	if (err) { throw err; }
+	server.listen(port, address, function() {
+	  var addr = this.address();
+	  console.log('listening on %s:%d in %s mode', addr.address, addr.port, env);
+	});
+});
