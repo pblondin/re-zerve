2a693b703221aef34cd719a0ef84f1a6b3811487
Philippe Blondin <blondin.philippe@gmail.com>
2013-11-22 11:40:35 -0500

    Clean up code

--- app/controllers/restaurants_controller.js
+++ app/controllers/restaurants_controller.js
@@ -1,15 +1,14 @@
-var locomotive = require('locomotive');
-var mongoose = require('mongoose');
-var Controller = locomotive.Controller;
-
-var ObjectId = mongoose.Schema.Types.ObjectId;
-
+// Models
 var Restaurant = require('../models/restaurant');
 var SurveyCode = require('../models/surveycode');
-var couponCode = require('coupon-code');
-var RestaurantController = new Controller();
- 
- 
+
+// Utils
+var db = require('../utils/db');
+var coupon = require('coupon-code');
+
+// Controller
+var RestaurantController = new (require('locomotive')).Controller();
+
 //TODO: Add authentication
 
 /*
@@ -34,46 +33,51 @@ var RestaurantController = new Controller();
 // Index (GET /restaurants)
 RestaurantController.index = function() {
 	var self = this;
-	
-	fetchResource({}, self.res, function(err, restaurants) {
-	  (err) ? self.next(err) : self.res.json(restaurants);
-	});
+
+    db.list(Restaurant, function(err, restaurants) {
+        if (err) {
+            self.next(err);
+        }
+        else {
+            self.res.format({
+                'text/html': function() {
+                    self.render('index', {
+                        'title': "Show all restaurants",
+                        'restaurants': restaurants
+                    });
+                },
+                'application/json': function() {
+                    self.res.json(restaurants);
+                }
+            })
+        }
+    })
 };
 
-//RestaurantController.index = function() {
-//  var self = this;
-//  
-//  function updateRestaurants(restaurants) {
-//	  console.log("update restaurants");
-//	  console.log("length: " + restaurants.length);
-//	  /*for (var i = 0; i < restaurants.length; i++) {
-//		  restaurants[i].getNumValidSurveys(function (err, count) {
-//			  console.log("count: " + count);
-//			  restaurants[i]['num_valid_surveys'] = count;
-//		  });  
-//	  }*/
-//	  self.render('index', {
-//		  'title': "Show all restaurants",
-//		  'restaurants': restaurants});
-//  }
-// 
-//  fetchResource({}, self.res, function(err, restaurants) {
-//	  if (err)
-//		  self.next(err);
-//	  else {
-//		  updateRestaurants(restaurants);
-//	  }
-//  });
-//};
-
-//Show (GET  /restaurants/:id)
+// Show (GET  /restaurants/:id)
 RestaurantController.show = function() {
   var self = this;
-  fetchResource({_id: self.param('id')}, self.res, function(err, restaurant) {
-    (err) ? self.next(err) : self.render('show', {
-    	'title': "View a restaurant",
-    	'restaurant': restaurant[0]
-    });
+
+  db.del(Restaurant, self.param('id'), function(err, restaurant) {
+      if (err) {
+          self.next(err);
+      }
+      else if (!restaurant) { // Restaurant not found.
+          self.res.send('Restaurant not found');
+      }
+      else {
+          self.res.format({
+              'text/html': function() {
+                  self.render('show', {
+                      'title': "Show a restaurant",
+                      'restaurant': restaurant
+                  });
+              },
+              'application/json': function() {
+                  self.res.json(restaurant);
+              }
+          })
+      }
   });
 };
 
@@ -81,7 +85,7 @@ RestaurantController.show = function() {
 RestaurantController.new = function() { 
   var self = this;
   self.render('new', {
-	  title		: 'New restaurant'
+	  'title': 'New restaurant'
   });
 };
 
@@ -89,46 +93,48 @@ RestaurantController.new = function() {
 RestaurantController.create = function() {
   var self = this;
 
-  var restaurant = new Restaurant({
-	  name               : self.param('name'),
-	  owner_name         : self.param('owner_name'),
-	  reservation_email  : self.param('reservation_email'),
-	  survey_url         : self.param('survey_url')
-	  });
-  restaurant.save(function(err) {
-	  (err) ? self.next(err) : self.res.redirect(self.urlFor({ action: 'index' }));
+  var data = {
+      name               : self.param('name'),
+      owner_name         : self.param('owner_name'),
+      reservation_email  : self.param('reservation_email'),
+      survey_url         : self.param('survey_url')
+  };
+
+  db.post(Restaurant, data, function(err, restaurant) {
+      (err) ? self.next(err) : self.res.redirect(self.urlFor({ action: 'index' }));
   });
 };
 
 // Edit (GET /restaurants/edit)
 RestaurantController.edit = function() { 
 	var self = this;
-	fetchResource({}, self.res, function(err, restaurants) {
-	   (err) ? self.next(err) : self.render('edit', {
-										title		: 'Edit a restaurant',
-										restaurants	: restaurants
-									});
-	});
+
+    db.list(Restaurant, function(err, restaurants) {
+        (err) ? self.next(err) : self.render('edit', {'title': 'Edit a restaurant',
+                                                      'restaurants': restaurants});
+    });
 };
 
 // Update (PUT /restaurants/:id)
 RestaurantController.update = function() { 
   var self = this;
-  updateResource(self.param('id'), self.req.body, self.res, function(err, restaurant) {
-    (err) ? self.next(err) : self.res.send(restaurant);
+
+  db.put(Restaurant, self.param('id'), self.req.body, function(err, restaurant) {
+      (err) ? self.next(err): self.res.send(restaurant);
   });
 };
 
 // Destroy (DELETE /restaurants/:id)
 RestaurantController.destroy = function() {
   var self = this;
-  Restaurant.findByIdAndRemove(self.param('id'), function(err, restaurant) {
-    if (err)
-      self.next(err);
-    else if (!restaurant)
-      self.res.send('Restaurant not found.');
-    else
-      self.res.send('Restaurant successfully deleted.');
+
+  db.del(Restaurant, self.param('id'), function(err, restaurant) {
+      if (err)
+          self.next(err);
+      else if (!restaurant)
+          self.res.send('Restaurant not found.');
+      else
+          self.res.send('Restaurant successfully deleted.');
   });
 };
 
@@ -141,9 +147,10 @@ RestaurantController.search = function() {
 	  if( query.hasOwnProperty(k) ) {
 		  query[k] = new RegExp(query[k], "i"); // Find case-insensitive match
 	  }
-  }  
-  fetchResource(query, self.res, function(err, restaurants) {
-    (err) ? self.next(err) : self.res.json(restaurants);
+  }
+
+  db.search(Restaurant, query, function(err, restaurants) {
+      (err) ? self.next(err) : self.res.json(restaurants);
   });
 };
 
@@ -170,47 +177,23 @@ RestaurantController.generatecodes = function() {
   var self = this;
   var number_of_codes = 10;
 
-  fetchResource({_id: self.param('id')}, self.res, function(err, restaurant) {
-    if (err)
-      self.next(err);
-    else {
-      var number = (! self.param('n')) ? number_of_codes : self.param('n');
-      for (var i = 1; i <= number; i++) {
-        var cc = couponCode.generate({ parts: 2});
-        var my_code = new SurveyCode({code: cc, rest_ref: restaurant[0]._id });
-        my_code.save(function(err){	
-          if (err) return next(err); //self.res.send('Error saving the survey code.');
-        });
-        restaurant[0].survey_codes.push(my_code._id);
-      } 
-      restaurant[0].save(function(err) {
-        (err) ? self.next(err) : self.res.send(restaurant[0]);
-      });
-    }
-  });
-};
-
-
-//Utility function
-fetchResource = function(params, res, cb) {
-  Restaurant.find(params, function(err, restaurant) {
-    if (err)
-      return cb(err, null);
-    else if (!restaurant || restaurant.length == 0)
-      return cb(null, []);	// Restaurant not found.
-    else
-      return cb(null, restaurant);
-  });
-};
-
-updateResource = function(id, update, res, cb) {
-  Restaurant.findByIdAndUpdate(id, update, function(err, restaurant) {
-    if (err)
-      return cb(err, null);
-    else if (!restaurant || restaurant.length == 0)
-      return cb(null, []);	// Restaurant not found.
-    else
-      return cb(null, restaurant);
+  db.get(Restaurant, self.param('id'), function(err, restaurant) {
+      if (err)
+          self.next(err);
+      else {
+          var number = (! self.param('n')) ? number_of_codes : self.param('n');
+          for (var i = 1; i <= number; i++) {
+              var cc = coupon.generate({ parts: 2});
+              var my_code = new SurveyCode({code: cc, rest_ref: restaurant[0]._id });
+              my_code.save(function(err){
+                  if (err) self.next(err); //self.res.send('Error saving the survey code.');
+              });
+              restaurant[0].survey_codes.push(my_code._id);
+          }
+          restaurant[0].save(function(err) {
+              (err) ? self.next(err) : self.res.send(restaurant[0]);
+          });
+      }
   });
 };
 
